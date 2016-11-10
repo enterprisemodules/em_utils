e6266affe4e4b41e5a2a5dfe3db8503bb52a3397
Bert Hajee <bert.hajee@enterprisemodules.com>
2016-11-10 17:55:42 +0100

    [license::available] Use the puppet_confdir fact to decide where to put the entitlements
    This change allows this code to work whatever version or custom paths the agent is using.

--- manifests/license/available.pp
+++ manifests/license/available.pp
@@ -1,7 +1,10 @@
 # Class: em_utils::license::available
 #
 # This class makes sure all the licenses installed on the server are available on all agents
-# The modules from Enterprise Modules need them on the agents. 
+# The modules from Enterprise Modules need them on the agents.
+#
+# The puppet_confdir fact contains the value of the Puppet setting confdir from the puppet.conf
+# settings file. So this class copies all entitlemnets to the puppet config directory.
 #
 class em_utils::license::available(
   $server = '',
@@ -10,7 +13,7 @@ class em_utils::license::available(
 
   file { 'EM entitlements':
     ensure  => 'directory',
-    path    => "/etc/puppetlabs/puppet/${path}",
+    path    => "${puppet_confdir}/${path}",
     source  => "puppet://${server}/modules/${path}",
     recurse => true,
   }
